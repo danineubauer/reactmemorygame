28912eb50f95428da277480b971b9ca6120a8de0
Danielle <danineubauerr@gmail.com>
2020-03-11 20:05:47 -0700

    maps array of photos

--- myapp/src/components/PhotoGrid/index.js
+++ myapp/src/components/PhotoGrid/index.js
@@ -5,32 +5,48 @@ import Container from "../Container"
 // import MemoryCard from "../MemoryCard"
 import cardsData from "../../cards.json"
 import MemoryCard from '../MemoryCard';
+import uuid from "uuid/v1"
+import "./style.css"
 
 
 const PhotoGrid = () => {
+
     const [cards, setCards] = useState(cardsData)
 
     const cardClicked = event => {
         console.log('card clicked: ', event.target); //loging the photo info clicked
-        console.log(Math.floor(Math.random()*13)) 
-        // const newShuffle = cards.(card => cardsData.id !== id)
-        // setCards(newShuffle);
+
+        console.log('cardsData', cardsData)
+
+        
+
+        shufflePhotoDisplay();
+    }
+    
+    const shufflePhotoDisplay = () => {
+        const shuffle = require('shuffle-array'), 
+            collection = cards;
+        shuffle(cards);
+        console.log("cards", cards);
+        console.log(cards.length)
+        setCards([...cards, shuffle(cards)])
     }
 
     return (
         <Wrapper>
-            <Container>
-                {cards.map(card =>
-                    <MemoryCard
-                        image={card.image}
-                        id={card.id}
-                        onClick={cardClicked}
-                    />
-                )}
-              </Container>
+                <Container>
+                        {cards.map(card => { 
+                            return (
+                                <img key={uuid()} id={card.id} src={card.image} onClick={cardClicked}/>
+                            )
+                        })
+                        }
+                </Container>
         </Wrapper>
     )
+    }
 
-}
+
+
 
 export default PhotoGrid; 
