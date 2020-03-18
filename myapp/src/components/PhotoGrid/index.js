fd1544c97ff12fc72e5a54729db42a6f16c510f6
Danielle <danineubauerr@gmail.com>
2020-03-18 16:52:34 -0700

    styles sheet

--- myapp/src/components/PhotoGrid/index.js
+++ myapp/src/components/PhotoGrid/index.js
@@ -12,30 +12,31 @@ import "./style.css"
 const PhotoGrid = () => {
 
     const [cards, setCards] = useState(cardsData)
+    console.log('cardsData', cardsData)
 
     const cardClicked = event => {
         console.log('card clicked: ', event.target); //loging the photo info clicked
 
         console.log('cardsData', cardsData)
 
-        
-
         shufflePhotoDisplay();
     }
     
+    //shufles photo:
     const shufflePhotoDisplay = () => {
         const shuffle = require('shuffle-array'), 
             collection = cards;
-        shuffle(cards);
-        console.log("cards", cards);
-        console.log(cards.length)
-        setCards([...cards, shuffle(cards)])
+            console.log("cards", cards);
+            // console.log("cards", cards2);
+             console.log(cards.length)
+             setCards(shuffle(cards, { 'copy': true }))
     }
+    
 
     return (
         <Wrapper>
                 <Container>
-                        {cards.map(card => { 
+                        {cards.map(card =>  { 
                             return (
                                 <img key={uuid()} id={card.id} src={card.image} onClick={cardClicked}/>
                             )
@@ -45,8 +46,5 @@ const PhotoGrid = () => {
         </Wrapper>
     )
     }
-
-
-
 
 export default PhotoGrid; 
